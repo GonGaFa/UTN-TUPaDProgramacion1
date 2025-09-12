e1737fbb93729f7d79fcc7489722af4d7d1a2c2d
GonGaFa <gonzalogarrido@mi.unc.edu.ar>
2025-09-11 23:20:00 -0300

    Actividades realizadas (En proceso)
    Trabajo en proceso de las actividades, se creo un archivo de prueba para realizacion de los ejercicios para su correcta implementacion.
    Aparte de eso se creo el archivo de la practica 5 para adelantarme a la otra practica

---
+++ Unidad 4/Practica 4, repetitivas.py
@@ -0,0 +1,13 @@
+#Ejercicio 1
+
+for i in range(1, 101):
+    print(i)
+
+#Ejercicio 2
+
+numero = (input("Ingrese un número: "))
+
+cantidad = len(numero)
+
+print("El número tiene ", cantidad , " de digitos.")
+
---
+++ Unidad 4/prueba.py
@@ -0,0 +1,10 @@
+#ejercicio 3
+"Escribe un programa que sume todos los números enteros comprendidos entre dos valores dados por el usuario, excluyendo esos dos valores."
+
+numero_max= int(input("Ingrese un numero: "))
+
+numero_min = int(input("Ingrese otro numero: "))
+
+for i in range (numero_min, numero_max):
+    if numero_min == numero_min:
+        print("la sumatoria de todos numero es" )
