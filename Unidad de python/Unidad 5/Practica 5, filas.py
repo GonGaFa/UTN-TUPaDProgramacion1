aa6e91aa831e0b9a636424345d1255b52a261fba
GonzaloGarridoFas <gonzalogarrido@mi.unc.edu.ar>
2025-09-23 19:43:20 -0300

    Ejercicios practico5

---
+++ Unidad de python/Unidad 5/Practica 5, filas.py
@@ -0,0 +1,53 @@
+#Ejercicio 1
+
+notas = [2,5,10,6,4,7,4,5,2,1]
+
+print("Las notas son: ", notas)
+
+promedio = sum(notas)/len(notas)
+print("El promedio es: ", promedio)
+
+#Ejercicio 2
+
+
+
+#Ejercicio 3
+
+
+
+#Ejercicio 4
+
+
+
+#Ejercicio 5
+
+nombres=["Ana","Luis","Carlos","Marta","Lucia","Jorge","Sofia","Diego"]
+
+print("Los nombres son: ", nombres)
+input(print("Si deseas agregar un nombre, ingresa 'Agregar', de lo contrario, si deseas quitar alguno ingresa 'remover': "))
+if str == "Agregar":
+    persona = input("Ingresa el nombre que deseas agregar: ")
+    nombres.append(persona)
+elif str == "remover":
+    persona = input("Ingresa el nombre que deseas eliminar: ")
+    nombres.remove(persona)
+
+print("Los nombres son: ", nombres)
+
+#Ejercicio 6
+
+
+
+#Ejercicio 7
+
+
+
+#Ejercicio 8
+
+
+
+#Ejercicio 9
+
+
+
+#Ejercicio 10
